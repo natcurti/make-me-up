4837f06a5c80b1e5a07cd054859f2ca7063bd889
natcurti <naticurti01@gmail.com>
2024-04-15 18:11:52 -0300

    Initialize project using vite and install bootstrap

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.js";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
