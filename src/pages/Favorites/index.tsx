592187dc6b97c1c976eab8736cf46459312f5c1e
natcurti <naticurti01@gmail.com>
2024-05-01 18:01:46 -0300

    Adjust on favorite's page

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -11,7 +11,7 @@ const Favorites = () => {
   return (
     <div className="container-xxl mt-5 mx-auto d-flex flex-column align-items-center">
       <h3>Meus Favoritos</h3>
-      <main>
+      <main className="w-100">
         <ContainerProducts>
           {favoriteProducts.map((item) => (
             <Product key={item.id} {...item} />
